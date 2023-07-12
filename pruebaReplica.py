11e4e658c9efb8c84304df548ec3422f8845895a
Fernando Medina Palacios <96751939+fernanclok@users.noreply.github.com>
2023-07-11 22:38:09 -0700

    ReplicaMongoDB.py

--- pruebaReplica.py
+++ pruebaReplica.py
@@ -3,33 +3,33 @@ import subprocess
 import time
 import os
 
-# Configuración de la conexión a MongoDB Atlas
+# Configuración de la conexión a MongoDB Atlas // Configuration of the connection to MongoDB Atlas
 atlas_connection_string = "mongodb+srv://PruebaWeb:pruebaweb@cluster0.lolgdgf.mongodb.net/?retryWrites=true&w=majority"
 atlas_client = MongoClient(atlas_connection_string)
-atlas_db = atlas_client["test"]
-atlas_collection = atlas_db["users"]
+atlas_db = atlas_client["test"] # nombre de la base de datos // name of the database
+atlas_collection = atlas_db["users"] # nombre de la coleccion // name of the collection
 
-# Configuración de la conexión a MongoDB Compass
+# Configuración de la conexión a MongoDB Compass // Configuration of the connection to MongoDB Compass
 compass_connection_string = "mongodb://127.0.0.1:27017"
 compass_client = MongoClient(compass_connection_string)
-compass_db = compass_client["replicacion1"]
-compass_collection = compass_db["replicas"]
+compass_db = compass_client["replicacion1"] # nombre de la base de datos // name of the database
+compass_collection = compass_db["replicas"] # nombre de la coleccion // name of the collection
 
-# Función para replicar los datos
+# Función para replicar los datos // function to replicate our data
 def replicate_data():
-    # Obtener los documentos de MongoDB Atlas
+    # Obtener los documentos de MongoDB Atlas // get all the documents form MongoDB Atlas
     documents = list(atlas_collection.find())
 
     if len(documents) > 0:
-        # Exportar los datos a un archivo de respaldo
-        backup_file = "replica.txt"
+        # Exportar los datos a un archivo de respaldo // export the data to a backup file
+        backup_file = "replica.txt" # nombre de nuestro archivo donde se guardara los datos // name our your file when whe are exportin the data 
         subprocess.run(["mongodump", "--uri", atlas_connection_string, "--archive=" + backup_file])
 
-        # Verificar si el archivo de respaldo se generó correctamente
+        # Verificar si el archivo de respaldo se generó correctamente // validate if the backup file exists
         if os.path.exists(backup_file) and os.path.getsize(backup_file) > 0:
             print("El archivo de respaldo se generó correctamente.")
 
-            # Importar los datos en MongoDB Compass
+            # Importar los datos en MongoDB Compass // import the data to MongoDB Compass
             subprocess.run(["mongorestore", "--uri", compass_connection_string, "--archive=" + backup_file, "--nsFrom=test.users", "--nsTo=replicacion1.replicas"])
             print("Los datos se importaron correctamente en MongoDB Compass.")
         else:
@@ -37,7 +37,7 @@ def replicate_data():
     else:
         print("No hay nuevos documentos para replicar.")
 
-# Replicar los datos cada cierto intervalo de tiempo (por ejemplo, cada 5 minutos)
+# Replicar los datos cada cierto intervalo de tiempo (por ejemplo, cada 2 minutos) // replicate the data every 2 minutes
 while True:
     replicate_data()
-    time.sleep(2 * 60)  # Esperar 2 minutos antes de la siguiente replicación
+    time.sleep(2 * 60)  # Esperar 2 minutos antes de la siguiente replicación // wait 2 minutes and do it the replication again
